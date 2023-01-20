6cc1b3ed7e897006ff1a69cba40000da3ddc0f3e
mrotskoff <mrotskoff@gmail.com>
2023-01-19 20:46:15 -0800

    fixed logging

--- cbpi4_PIDWithTimeProportionalControl/__init__.py
+++ cbpi4_PIDWithTimeProportionalControl/__init__.py
@@ -37,44 +37,44 @@ class PIDWithTimeProportionalControl(CBPiKettleLogic):
             pid = PIDArduino(sample_period, p, i, d, 0, 100)
 
             while self.running == True:
-                self._logger.debug('--- entering PID loop ---')
+                logging.debug('--- entering PID loop ---')
 
-                self._logger.debug("P input:      {0}".format(p))
-                self._logger.debug("I input:      {0}".format(i))
-                self._logger.debug("D input:      {0}".format(d))
+                logging.debug("P input:      {0}".format(p))
+                logging.debug("I input:      {0}".format(i))
+                logging.debug("D input:      {0}".format(d))
 
                 sensor_value = current_temp = self.get_sensor_value(self.kettle.sensor).get("value")
                 target_temp = self.get_kettle_target_temp(self.id)
-                self._logger.debug('sensor temp:  {0}'.format(sensor_value))
-                self._logger.debug('target temp:  {0}'.format(target_temp))
+                logging.debug('sensor temp:  {0}'.format(sensor_value))
+                logging.debug('target temp:  {0}'.format(target_temp))
 
                 pid_calc = pid.calc(sensor_value, target_temp)
             
                 on_time = sample_period * (pid_calc / 100)
-                self._logger.debug('on_time:      {0}'.format(on_time))
+                logging.debug('on_time:      {0}'.format(on_time))
                 
                 if on_time < min_on_time:
-                    self._logger.debug('on_time below minimum {0}'.format(min_on_time))
+                    logging.debug('on_time below minimum {0}'.format(min_on_time))
                     on_time = 0
                 
                 off_time = sample_period - on_time
-                self._logger.debug('off_time:     {0}'.format(off_time))
+                logging.debug('off_time:     {0}'.format(off_time))
             
                 if on_time > 0:
-                    self._logger.debug('turning heater on for {0} seconds'.format(on_time))
+                    logging.debug('turning heater on for {0} seconds'.format(on_time))
                     await self.actor_on(self.heater)
                     await asyncio.sleep(on_time)
                 if off_time > 0:
-                    self._logger.debug('turning heater off for {0} seconds'.format(off_time))
+                    logging.debug('turning heater off for {0} seconds'.format(off_time))
                     await self.actor_off(self.heater)
                     await asyncio.sleep(off_time)
                                     
-                self._logger.debug('--- exiting PID loop  ---')
+                logging.debug('--- exiting PID loop  ---')
 
         except asyncio.CancelledError as e:
             pass
         except Exception as e:
-            self._logger.error("PIDWithTimeProportionalControl Error {}".format(e))
+            logging.error("PIDWithTimeProportionalControl Error {}".format(e))
         finally:
             self.running = False
             await self.actor_off(self.heater)
@@ -96,7 +96,6 @@ class PIDArduino(object):
         if outputMin >= outputMax:
             raise ValueError('outputMin must be less than outputMax')
 
-        self._logger = logging.getLogger(type(self).__name__)
         self._Kp = kp
         self._Ki = ki * sampleTimeSec
         self._Kd = kd / sampleTimeSec
@@ -143,10 +142,10 @@ class PIDArduino(object):
         self._lastOutput = max(self._lastOutput, self._outputMin)
 
         # Log some debug info
-        self._logger.debug('P output:   {0}'.format(p))
-        self._logger.debug('I output:   {0}'.format(i))
-        self._logger.debug('D output:   {0}'.format(d))
-        self._logger.debug('PID output: {0}'.format(self._lastOutput))
+        logging.debug('P output:   {0}'.format(p))
+        logging.debug('I output:   {0}'.format(i))
+        logging.debug('D output:   {0}'.format(d))
+        logging.debug('PID output: {0}'.format(self._lastOutput))
 
         # Remember some variables for next time
         self._lastInput = inputValue
